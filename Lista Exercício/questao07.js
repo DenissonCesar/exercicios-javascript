e88df50876ec5bcb48622d43d1c4910533b3a2ed
Denisson César <denissoncesar6@gmail.com>
2025-03-14 14:44:56 -0300

    Sete exercícios resolvidos

---
+++ Lista Exercício/questao07.js
@@ -0,0 +1,24 @@
+function analisarDesempenho(pontuacoes) {
+    let jogos = pontuacoes.split(" ").map(Number)
+    let recordes = 0
+    let piorJogo = 0
+    let melhorPontuacao = jogos[0]
+    let piorPontuacao = jogos[0]
+
+    for (let i = 1; i < jogos.length; i++) {
+        if (jogos[i] > melhorPontuacao) {
+            melhorPontuacao = jogos[i]
+            recordes++
+        }
+        if (jogos[i] < piorPontuacao) {
+            piorPontuacao = jogos[i]
+            piorJogo = i + 1
+        }
+    }
+
+    return [recordes, piorJogo]
+}
+
+
+let stringPontuacoes = "10 20 20 8 25 3 0 30 1"
+console.log(analisarDesempenho(stringPontuacoes))
